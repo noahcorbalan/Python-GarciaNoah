c82f7ee6fec3c2aae6ae47de0212df818c049d09
noahcorbalan <71399427+noahcorbalan@users.noreply.github.com>
2021-01-20 22:02:07 +0100

    Update Python - Programa amb la funció round().py

--- Python - Programa amb la funció round().py
+++ Python - Programa amb la funció round().py
@@ -2,3 +2,5 @@ nota=float(input('Introdueix la teva nota:'))
 print ('La teva nota es:',round(nota))
 nota_angles=float(input('Introdueix la teva nota:'))
 print ('La teva nota es:',round(nota_angles))
+nota_final=(nota+nota_angles)/2
+print ('la teva nota final es:',nota_final)
