906768016fd7cb60841f5e54a6342c0f0db49303
noahcorbalan <71399427+noahcorbalan@users.noreply.github.com>
2021-01-20 20:21:02 +0100

    Create Python - Programa amb la funció round().py

---
+++ Python - Programa amb la funció round().py
@@ -0,0 +1,4 @@
+nota=float(input('Introdueix la teva nota:'))
+print ('La teva nota es:',round(nota))
+nota_angles=float(input('Introdueix la teva nota:'))
+print ('La teva nota es:',round(nota_angles))
